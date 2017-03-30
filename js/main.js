49b310b328d0671b547dade78d87226b4ba6d860
Collet Lauriane <lauriane.collet1@gmail.com>
2017-03-30 17:03:54 +0200

    Add a new index1.html and i changed my main.js for movies display

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ $(document).ready(function () {
 		$.get(url, function (data) {
 			let results = data.Search
 			for (let i = 0; i < results.length; i++) {
-
+				$('#affichage').append(`<li>${results[i].Title}</li>`);
+				$('#affichage').append(`<li>${results[i].Type}</li>`);
+				$('#affichage').append(`<li>${results[i].Year}</li>`);
+				$('#affichage').append(`<li>${results[i].imdbID}</li>`);
+				$('#affichage').append(`<li>${results[i].Image}</li>`);
 			}
 
 		});
