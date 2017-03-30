876cf15793ffda5a0d872a73b97136891ca79fb5
zoemhart <zoemhrt@gmail.com>
2017-03-30 18:26:25 +0200

    fixed js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
 let appli = new Vue({
   el: '#app',
   data: {
+  	keyword: '',
     Search: []
-  }
-});
+  },
 
+  methods: {
+  	rechercher: function(item){
+	let url = `https://www.omdbapi.com/?s=${this.keyword}`;
 
-$.getJSON("http://www.omdbapi.com/?t=c&y=2001", function( data ) {
-	appli.Search = data;
-	console.log(data);
-
-});
-
-$(document).ready(function () {
-
-	$('#search_button').click(function () {
-		let movie = $('#film').val();
-		let url = 'https://www.omdbapi.com/?s=' + movie;
-
-
-		$.get(url, function (data) {
-			let results = data.Search
-			for (let i = 0; i < results.length; i++) {
-				$('#affichage').append(`<li>${results[i].Title}</li>`);
-				$('#affichage').append(`<li>${results[i].Type}</li>`);
-				$('#affichage').append(`<li>${results[i].Year}</li>`);
-				$('#affichage').append(`<li>${results[i].imdbID}</li>`);
-				$('#affichage').append(`<li>${results[i].Image}</li>`);
-			}
-
+	$.getJSON(url, function( data ) {
+		 appli.Search = data.Search;
+		 console.log(data.Search);
 		});
 
+
+}
+}
+
 });
+
+
+
+
+
